ee46282bd722ee4668d23e0f5340b577093358d2
Jean-Claude Beaudoin <jean.claude.beaudoin@gmail.com>
2017-01-06 01:55:02 -0500

    Try to make sure BT compiles properly on MKCL pre 1.1.10.

--- contrib/bordeaux-threads-0.8.5/src/impl-mkcl.lisp
+++ contrib/bordeaux-threads-0.8.5/src/impl-mkcl.lisp
@@ -16,38 +16,48 @@ Distributed under the MIT license (see LICENSE file)
 
 (defun %make-thread (function name)
   (mt:thread-run-function name function))
+(declaim (notinline %make-thread))
 
 (defun current-thread ()
   mt::*thread*)
+(declaim (notinline current-thread))
 
 (defun threadp (object)
   (typep object 'mt:thread))
+(declaim (notinline threadp))
 
 (defun thread-name (thread)
   (mt:thread-name thread))
+(declaim (notinline thread-name))
 
 ;;; Resource contention: locks and recursive locks
 
 (defun make-lock (&optional name)
   (mt:make-lock :name (or name "Anonymous lock")))
+(declaim (notinline make-lock))
 
 (defun acquire-lock (lock &optional (wait-p t))
   (mt:get-lock lock wait-p))
+(declaim (notinline acquire-lock))
 
 (defun release-lock (lock)
   (mt:giveup-lock lock))
+(declaim (notinline release-lock))
 
 (defmacro with-lock-held ((place) &body body)
   `(mt:with-lock (,place) ,@body))
 
 (defun make-recursive-lock (&optional name)
   (mt:make-lock :name (or name "Anonymous recursive lock") :recursive t))
+(declaim (notinline make-recursive-lock))
 
 (defun acquire-recursive-lock (lock &optional (wait-p t))
   (mt:get-lock lock wait-p))
+(declaim (notinline acquire-recursive-lock))
 
 (defun release-recursive-lock (lock)
   (mt:giveup-lock lock))
+(declaim (notinline release-recursive-lock))
 
 (defmacro with-recursive-lock-held ((place) &body body)
   `(mt:with-lock (,place) ,@body))
@@ -57,24 +67,29 @@ Distributed under the MIT license (see LICENSE file)
 (defun make-condition-variable (&key name)
   (declare (ignore name))
   (mt:make-condition-variable))
+(declaim (notinline make-condition-variable))
 
 (defun condition-wait (condition-variable lock &key timeout)
   (signal-error-if-condition-wait-timeout timeout)
   (mt:condition-wait condition-variable lock)
   t)
+(declaim (notinline condition-wait))
 
 (define-condition-wait-compiler-macro)
 
 (defun condition-notify (condition-variable)
   (mt:condition-signal condition-variable))
+(declaim (notinline condition-notify))
 
 (defun thread-yield ()
   (mt:thread-yield))
+(declaim (notinline thread-yield))
 
 ;;; Introspection/debugging
 
 (defun all-threads ()
   (mt:all-threads))
+(declaim (notinline all-threads))
 
 (defun interrupt-thread (thread function &rest args)
   (flet ((apply-function ()
@@ -83,15 +98,19 @@ Distributed under the MIT license (see LICENSE file)
                function)))
     (declare (dynamic-extent #'apply-function))
     (mt:interrupt-thread thread (apply-function))))
+(declaim (notinline interrupt-thread))
 
 (defun destroy-thread (thread)
   (signal-error-if-current-thread thread)
   (mt:thread-kill thread))
+(declaim (notinline destroy-thread))
 
 (defun thread-alive-p (thread)
   (mt:thread-active-p thread))
+(declaim (notinline thread-alive-p))
 
 (defun join-thread (thread)
   (mt:thread-join thread))
+(declaim (notinline join-thread))
 
 (mark-supported)
