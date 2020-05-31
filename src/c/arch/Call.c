642acdb69019789efa23399f2b5e571ccb85c249
Jean-Claude Beaudoin <jean.claude.beaudoin@gmail.com>
2020-05-31 06:59:27 -0400

    Add some more cases.

--- src/c/arch/Call.c
+++ src/c/arch/Call.c
@@ -34,14 +34,16 @@ void old_call_9(void)
 }
 
 extern float f0, f1, f2, f3, f4, f5, f6, f7, f8;
-extern double d0, d1, d2, d3, d4, d5, d6, d7, d8;
+extern double d0, d1, d2, d3, d4, d5, d6, d7, d8, d9;
 
 
 extern void callee_2f_2i(float f0, float f1, long i0, long i1, long i2, long i3, long i4);
 extern void callee_2d_2i(double d0, double d1, long i0, long i1, long i2, long i3, long i4);
 extern void old_callee_2f_2i();
 extern void old_callee_2d_2i();
-extern void var_callee_i_2f_2i(long i0, ...);
+extern void callee_i_2f_4i(long i0, float f0, float f1, int i1, long i2, long i3, long i4);
+extern void old_callee_i_2f_4i();
+extern void var_callee_i_2f_4i(long i0, ...);
 extern void callee_9d(double d0, double d1, double d2, double d3, double d4, double d5, double d6, double d7, double d8);
 extern void callee_9f(float f0, float f1, float f2, float f3, float f4, float f5, float f6, float f7, float f8);
 extern void old_callee_9f();
@@ -57,9 +59,19 @@ void old_call_2f_2i(void)
   old_callee_2f_2i(f0, f1, i0, i1, i2, i3, i4);
 }
 
-void var_call_i_2f_2i(void)
+void call_i_2f_4i(void)
 {
-  var_callee_i_2f_2i(i0, f0, f1, i1, i2, i3, i4);
+  callee_i_2f_4i(i0, f0, f1, i1, i2, i3, i4);
+}
+
+void var_call_i_2f_4i(void)
+{
+  var_callee_i_2f_4i(i0, f0, f1, i1, i2, i3, i4);
+}
+
+void old_call_i_2f_4i(void)
+{
+  old_callee_i_2f_4i(i0, f0, f1, i1, i2, i3, i4);
 }
 
 void call_2d_2i(void)
@@ -106,5 +118,12 @@ extern void callee_1l_1ll_2l(long i0, long long ll1, long l2, long l3);
 void call_1l_1ll_2l(void)
 {
   callee_1l_1ll_2l(i0, ll1, i2, i3);
+}
+
+extern void callee_9d_1f_1d_2f(double d0, double d1, double d2, double d3, double d4, double d5, double d6, double d7, double d8, float f9, double d10, float f11, float f12);
+
+void call_9d_1f_1d_2f(void)
+{
+  callee_9d_1f_1d_2f(d0, d1, d2, d3, d4, d5, d6, d7, d8, f0, d9, f1, f2);
 }
 
